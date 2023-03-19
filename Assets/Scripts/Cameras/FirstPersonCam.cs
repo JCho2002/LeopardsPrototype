b3bc08daa8bce76901dc86c1b6d7e3e230b8f898
giallo <114472976+gialloVero@users.noreply.github.com>
2023-03-19 21:43:25 +0100

    Impemented Camera
    -Still needs to implement player rotating when the first person camera is rotating.
    -many potential cut might inprove performances (the main camera lerps rotation and position every frame
    )

---
+++ Assets/Scripts/Cameras/FirstPersonCam.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCam : MonoBehaviour
+{
+    
+
+    [SerializeField]
+    private float _yLimit;
+    [SerializeField]
+    private float _rotationSpeed = 10;
+
+    private float _xRotation = 0;
+    private float _yRotation = 0;
+    
+
+    void Update()
+    {
+        _yRotation += Input.GetAxis("Horizontal")* _rotationSpeed*Time.deltaTime;
+        _xRotation += Input.GetAxis("Vertical") * _rotationSpeed * Time.deltaTime;
+
+        _xRotation = Mathf.Clamp(_xRotation, -_yLimit, _yLimit);
+
+        if (_yRotation > 359f)
+            _yRotation = 0f;
+        if (_yRotation < 0f)
+            _yRotation = 359f;
+
+        Debug.Log((_xRotation, " - ", _yRotation));
+        transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0f);
+
+        
+    }
+
+    public void ResetRotation() 
+    {
+        _xRotation = 0;
+        _yRotation = 0;
+    }
+}
---
+++ Assets/Scripts/Cameras/MainCamera.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float _lerpDuration = 200;
+    [SerializeField]
+    private GameObject[] _CamPOVs;
+    [SerializeField]
+    private FirstPersonCam cam1;
+
+
+    private float _timer = 0;
+    private int _position;
+    //private float _ratio;
+    private bool _isLerping = false;
+    private Vector3 _destinationPosition;
+    private Quaternion _destinationRotation;
+    private bool _pressed = false;
+
+    private void Start()
+    {
+        _destinationPosition = _CamPOVs[0].transform.position;
+        _destinationRotation = _CamPOVs[0].transform.rotation;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !_pressed) //change input to change the 
+            SwitchCamera();
+
+        if (_timer > 0)
+            Timer();
+
+        //if(_isLerping)
+        Lerp(_CamPOVs[_position].transform.position, _CamPOVs[_position].transform.rotation);
+    }
+
+    private void SwitchCamera()
+    {
+        _position++;
+        if (_position > _CamPOVs.Length - 1)
+            _position = 0;
+
+        _isLerping = true;
+        _timer = _lerpDuration;
+
+        cam1.ResetRotation();
+    }
+
+    private void Timer()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+            _isLerping = false;
+    }
+
+    private void Lerp(Vector3 position, Quaternion rotation)
+    {
+        float ratio = (_lerpDuration - _timer) / _lerpDuration;
+        transform.position = Vector3.Lerp(transform.position, position, ratio);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, ratio);
+    }
+}
